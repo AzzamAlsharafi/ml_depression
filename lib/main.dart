a82fd3d38f11aaf3145a029643a8d1d38a73a6ab
Azzam Alsharafi <azzamalsharafi@gmail.com>
2022-05-31 17:32:35 +0800

    Create circular progress bar
    Create circular progress bar with segments.
    This will be used to represent depression levels in each day.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,24 +31,87 @@ class HomeApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: CircleShape());
+    return const Center(child: CircleProgress(5, [0.0, 1, 0.4]));
   }
 }
 
-class CircleShape extends StatelessWidget {
-  const CircleShape({Key? key}) : super(key: key);
+// This widget represents a circular progress bar, that is divided into segments.
+// Accepts two parametrs: number of segments, a list of values, which is the percent of each segment.
+class CircleProgress extends StatelessWidget {
+  const CircleProgress(this.segment, this.percents, {Key? key})
+      : super(key: key);
 
-  static const size = 50.0;
+  final int segment;
+  final List<double> percents;
+
+  // Determines size of the circle.
+  static const circleRadius = 0.7;
+
+  // Thickness of the border. 1 = border covers the whole circle.
+  static const borderThickness = 0.2;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: size,
-      height: size,
-      decoration: const BoxDecoration(
-        color: Colors.blue,
-        shape: BoxShape.circle,
-      ),
+    return SfRadialGauge(
+      axes: [
+        // Create primary radial axis
+        RadialAxis(
+          minimum: 0,
+          maximum: segment.toDouble(),
+          interval: 1,
+          showLabels: false,
+          showTicks: false,
+          startAngle: 270,
+          endAngle: 270,
+          radiusFactor: circleRadius,
+          axisLineStyle: const AxisLineStyle(
+            thickness: borderThickness,
+            color: Color.fromARGB(30, 0, 169, 181),
+            thicknessUnit: GaugeSizeUnit.factor,
+          ),
+          ranges: [
+            GaugeRange(
+              startValue: 0,
+              endValue: segment.toDouble(),
+              color: Colors.blueGrey,
+              startWidth: 0.04,
+              endWidth: 0.04,
+              sizeUnit: GaugeSizeUnit.factor,
+            ),
+          ],
+          pointers: List<GaugePointer>.generate(
+            percents.length,
+            (index) => RangePointer(
+              value: (percents.length - index).toDouble(),
+              width: borderThickness,
+              sizeUnit: GaugeSizeUnit.factor,
+              pointerOffset: 0,
+              color: HSVColor.fromAHSV(
+                      1, percents[percents.length - 1 - index] * 100, 1, 1)
+                  .toColor(),
+            ),
+          ),
+        ),
+        // Create secondary radial axis for segmented line
+        RadialAxis(
+          minimum: 0,
+          maximum: segment.toDouble(),
+          interval: 1,
+          showLabels: false,
+          showTicks: true,
+          showAxisLine: false,
+          offsetUnit: GaugeSizeUnit.factor,
+          minorTicksPerInterval: 0,
+          startAngle: 270,
+          endAngle: 270,
+          radiusFactor: circleRadius,
+          majorTickStyle: const MajorTickStyle(
+              length: borderThickness,
+              lengthUnit: GaugeSizeUnit.factor,
+              thickness: 10,
+              color: Colors.white),
+        )
+      ],
     );
   }
 }
